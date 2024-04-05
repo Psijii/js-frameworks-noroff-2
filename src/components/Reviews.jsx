f18975dac539af042e34e1169f88480c9163566f
Siw Elin Årolilja Iversen <111748314+Psijii@users.noreply.github.com>
2024-04-05 10:14:29 +0200

    updating and proptyping reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,9 +5,23 @@
  * @returns {JSX.Element} - The rendered list of reviews.
  */
 import Review from "./Review";
+import PropTypes from 'prop-types';
 
 export default function Reviews({ reviews }) {
   return (
-    reviews && reviews.map((review) => <Review key={review.id} {...review} />)
+    reviews && reviews.length > 0 ? (
+      reviews.map((review) => <Review key={review.id} {...review} />)
+    ) : (
+      <p>No reviews available</p>
+    )
   );
 }
+
+Reviews.propTypes = {
+  reviews: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })).isRequired,
+};
