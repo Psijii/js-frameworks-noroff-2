dc6770b52b5b0d4863dfa117f2a3356521d25c01
Siw Elin Årolilja Iversen <111748314+Psijii@users.noreply.github.com>
2024-04-05 11:10:20 +0200

    minor updates overall

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -9,7 +9,9 @@
  */
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+
 import * as yup from "yup";
+
 import "./ContactPage.css";
 import styles from "../components/Button.module.css";
 
